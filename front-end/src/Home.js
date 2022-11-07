9c664d5c52d902d7aa294dd60acde763beceed76
Mkarthik1729 <mkarthik1729@gmail.com>
2022-11-07 23:15:54 +0530

    styled

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { CrazyButton, RedP } from './styles/general'
 
 function Home() {
   return (
     <div>
-      <Link to='client'>client</Link><br />
+      <CrazyButton>
+      <Link to='client'>client</Link>
+      </CrazyButton><br />
+      <CrazyButton>
       <Link to='doctor'>Doctor</Link><br />
+      </CrazyButton><br />
+      <CrazyButton>
       <Link to='Association'>Association</Link>
+      </CrazyButton>
+      <RedP>hdqwkhuekhUJIJN</RedP>
     </div>
   )
 }
---
+++ front-end/src/styles/general.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const RedP = styled.p`
+color: red;
+`
+
+export const CrazyButton = styled.button`
+color: aliceblue;
+border-radius: 20px;
+`
